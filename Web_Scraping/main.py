7ed833ad1e0ac2fc986584018b3bca678e0b152d
huLasPet <kenwoodo@hotmail.com>
2022-03-25 10:33:02 +0100

    Python Web related projects - working version

--- Web_Scraping/main.py
+++ Web_Scraping/main.py
@@ -7,6 +7,6 @@ site_data = soup.select(
     selector="html body.gp main#page-index div#middle div.container div.row div#center "
              "div.content-list.content-list-detailed ul.list-unstyled li.media div.media-body h4.media-heading a")
 
-print("Latest news from Gamepod:")
+print("Latest news from Gamepod.hu:")
 for article in site_data:
     print(f"{article.text} - https://gamepod.hu{article.get('href')}")
