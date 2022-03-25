7a44012a2eec7f5323843893c2569dc0c300cdb7
huLasPet <kenwoodo@hotmail.com>
2022-03-25 10:32:13 +0100

    Python Web related projects - initial

--- Web_Scraping/main.py
+++ Web_Scraping/main.py
@@ -1,8 +1,12 @@
-#TODO
-#   Try this first
-#   html body.gp main#page-index div#middle div.container div.row div#center div.content-list.content-list-detailed ul.list-unstyled li.media div.media-body h4.media-heading a
-#   Backup where the li[X] numbers need to change
-#   1.
-#   /html/body/main/div[2]/div/div/div[1]/div[2]/ul/li[1]/div/h4/a
-#   2.
-#   /html/body/main/div[2]/div/div/div[1]/div[2]/ul/li[2]/div/h4/a
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://gamepod.hu/index.html')
+soup = BeautifulSoup(response.text, "html.parser")
+site_data = soup.select(
+    selector="html body.gp main#page-index div#middle div.container div.row div#center "
+             "div.content-list.content-list-detailed ul.list-unstyled li.media div.media-body h4.media-heading a")
+
+print("Latest news from Gamepod:")
+for article in site_data:
+    print(f"{article.text} - https://gamepod.hu{article.get('href')}")
