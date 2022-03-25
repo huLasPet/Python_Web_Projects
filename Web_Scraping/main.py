825e8822e020f6b29566fed490d5d23e3c7902ff
huLasPet <kenwoodo@hotmail.com>
2022-03-25 08:30:47 +0100

    Python Web related projects - initial

---
+++ Web_Scraping/main.py
@@ -0,0 +1,8 @@
+#TODO
+#   Try this first
+#   html body.gp main#page-index div#middle div.container div.row div#center div.content-list.content-list-detailed ul.list-unstyled li.media div.media-body h4.media-heading a
+#   Backup where the li[X] numbers need to change
+#   1.
+#   /html/body/main/div[2]/div/div/div[1]/div[2]/ul/li[1]/div/h4/a
+#   2.
+#   /html/body/main/div[2]/div/div/div[1]/div[2]/ul/li[2]/div/h4/a
