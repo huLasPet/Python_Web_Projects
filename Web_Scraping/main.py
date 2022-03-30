4cb9eae08d35ffcb2c25d8513b7453899e46a0a7
huLasPet <kenwoodo@hotmail.com>
2022-03-30 10:58:48 +0200

    Python Web related projects - working version

--- Web_Scraping/main.py
+++ Web_Scraping/main.py
@@ -1,12 +1,24 @@
 import requests
+import asyncio
 from bs4 import BeautifulSoup
 
-response = requests.get('https://gamepod.hu/index.html')
-soup = BeautifulSoup(response.text, "html.parser")
-site_data = soup.select(
-    selector="html body.gp main#page-index div#middle div.container div.row div#center "
-             "div.content-list.content-list-detailed ul.list-unstyled li.media div.media-body h4.media-heading a")
+URLS = {"Gamepod": "https://gamepod.hu/index.html", "Prohardver": "https://prohardver.hu/index.html"}
+SELECTORS = {"Gamepod": "html body.gp main#page-index div#middle div.container div.row div#center "
+                        "div.content-list.content-list-detailed ul.list-unstyled li.media div.media-body "
+                        "h4.media-heading a",
+             "Prohardver": "html body.ph main#page-index-2 div#middle div.container div.row div#center "
+                           "div.content-list.content-list-compact.content-image-medium.content-image-right "
+                           "ul.list-unstyled li.media div.media-body h4.media-heading a"}
 
-print("Latest news from Gamepod.hu:")
-for article in site_data:
-    print(f"{article.text} - https://gamepod.hu{article.get('href')}")
+
+async def get_news():
+    pass
+
+
+for url in URLS:
+    print(f"\nLatest news from {url} - {URLS[url]}:")
+    response = requests.get(URLS[url])
+    soup = BeautifulSoup(response.text, "html.parser")
+    site_data = soup.select(selector=SELECTORS[url])
+    for article in site_data:
+        print(f"{article.text} - https://gamepod.hu{article.get('href')}")
