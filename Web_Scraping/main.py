b435eed598575ba432992a3ea182fc02b983cace
huLasPet <kenwoodo@hotmail.com>
2022-03-30 11:38:42 +0200

    Python Web related projects - working version

--- Web_Scraping/main.py
+++ Web_Scraping/main.py
@@ -1,5 +1,6 @@
 import requests
-import asyncio
+import cProfile
+import pstats
 from bs4 import BeautifulSoup
 
 URLS = {"Gamepod": "https://gamepod.hu/index.html", "Prohardver": "https://prohardver.hu/index.html"}
@@ -11,14 +12,22 @@ SELECTORS = {"Gamepod": "html body.gp main#page-index div#middle div.container d
                            "ul.list-unstyled li.media div.media-body h4.media-heading a"}
 
 
-async def get_news():
-    pass
+def get_news():
+    """Goes through the dict of pages where i want news from and prints out the top articles and their url."""
+    for url in URLS:
+        print(f"\nLatest news from {url} - {URLS[url]}:")
+        response = requests.get(URLS[url])
+        soup = BeautifulSoup(response.text, "html.parser")
+        site_data = soup.select(selector=SELECTORS[url])
+        for article in site_data:
+            print(f"{article.text} - https://gamepod.hu{article.get('href')}")
 
 
-for url in URLS:
-    print(f"\nLatest news from {url} - {URLS[url]}:")
-    response = requests.get(URLS[url])
-    soup = BeautifulSoup(response.text, "html.parser")
-    site_data = soup.select(selector=SELECTORS[url])
-    for article in site_data:
-        print(f"{article.text} - https://gamepod.hu{article.get('href')}")
+if __name__ == '__main__':
+    """Running get news and getting performance stats for the 10 top items ordered by time."""
+    with cProfile.Profile() as pr:
+        get_news()
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.print_stats(10)
