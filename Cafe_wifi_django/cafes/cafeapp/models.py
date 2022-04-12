a400b696ca283a2df5e97a7880cbcc3fb3b95f7b
huLasPet <kenwoodo@hotmail.com>
2022-04-12 13:19:20 +0200

    Cafe and Wi-Fi site, Django initial

---
+++ Cafe_wifi_django/cafes/cafeapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Cafe(models.Model):
+    name = models.CharField(max_length=250)
+    map_url = models.CharField(max_length=250)
+    img_url = models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
+    has_sockets = models.BooleanField(default=False)
+    has_toilet = models.BooleanField(default=False)
+    has_wifi = models.BooleanField(default=False)
+    can_take_calls = models.BooleanField(default=False)
+    seats = models.CharField(max_length=250)
+    coffee_price = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
--- Cafe_wifi_django/cafes/cafes/asgi.py
+++ Cafe_wifi_django/cafes/cafes/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Cafe_wifi_django project.
+ASGI config for cafes project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cafe_wifi_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cafes.settings')
 
 application = get_asgi_application()
