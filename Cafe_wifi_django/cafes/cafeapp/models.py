b7c302fd3ad84d991fee08bc32eb8961b76992b2
huLasPet <kenwoodo@hotmail.com>
2022-04-13 14:09:40 +0200

    Cafe and Wi-Fi site Django - added delete function

--- Cafe_wifi_django/cafes/cafeapp/models.py
+++ Cafe_wifi_django/cafes/cafeapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django import forms
+from wtforms import Form, SelectField, StringField, SubmitField
+from wtforms.validators import URL, DataRequired
 
 # Create your models here.
 
@@ -19,17 +21,17 @@ class Cafe(models.Model):
     def __str__(self):
         return self.name
 
+class CafeForm(Form):
+    """The form to use when adding or editing Cafes."""
+    name = StringField('Cafe name', validators=[DataRequired()])
+    map_url = StringField('Location - Google Maps link', validators=[DataRequired(), URL()])
+    location = StringField('Location name', validators=[DataRequired()])
+    img_url = StringField('Picture of the cafe', validators=[DataRequired(), URL()])
+    seats = StringField('# of seats', validators=[DataRequired()])
+    has_toilet = SelectField('Can use toilet', validators=[DataRequired()], choices=["True", "False"])
+    has_wifi = SelectField('Can use Wi-Fi', validators=[DataRequired()], choices=["True", "False"])
+    has_sockets = SelectField('Can charge device', validators=[DataRequired()], choices=["True", "False"])
+    coffee_price = StringField('Coffee price', validators=[DataRequired()])
+    can_take_calls = SelectField('Can take calls', validators=[DataRequired()], choices=["True", "False"])
+    submit = SubmitField('Submit')
 
-# class CafeForm(forms.Form):
-#     """The form to use when adding or editing Cafes."""
-#     name = forms.CharField(initial='Cafe name')
-#     map_url = forms.URLField(initial='Location - Google Maps link')
-#     location = forms.CharField(initial='Location name')
-#     img_url = forms.CharField(initial='Picture of the cafe')
-#     seats = forms.CharField(initial='# of seats')
-#     has_toilet = SelectField('Can use toilet')
-#     has_wifi = SelectField('Can use Wi-Fi')
-#     has_sockets = SelectField('Can charge device')
-#     coffee_price = forms.CharField(initial='Coffee price')
-#     can_take_calls = SelectField('Can take calls')
-#     submit = SubmitField()
--- Cafe_wifi_django/cafes/cafeapp/views.py
+++ Cafe_wifi_django/cafes/cafeapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import Cafe
+from .models import Cafe, CafeForm
+
 from django.http import HttpResponse
 
 
@@ -13,7 +14,27 @@ def index(request):
 
 
 def add(request):
-    return render(request, 'cafeapp/add.html')
+    form = CafeForm(request.POST)
+    if request.POST:
+        cafe_to_add = Cafe(can_take_calls=bool(form.can_take_calls.data),
+                           coffee_price=form.coffee_price.data,
+                           has_sockets=bool(form.has_sockets.data),
+                           has_toilet=bool(form.has_toilet.data),
+                           has_wifi=bool(form.has_wifi.data),
+                           img_url=form.img_url.data,
+                           location=form.location.data,
+                           map_url=form.map_url.data,
+                           name=form.name.data,
+                           seats=form.seats.data)
+        print(cafe_to_add)
+        return render(request, 'cafeapp/all.html')
+        # try:
+        #     db_sqlalchemy.session.add(cafe_to_add)
+        #     db_sqlalchemy.session.commit()
+        #     return redirect(url_for('cafes'))
+        # except sqlalchemy.exc.IntegrityError:
+        #     return jsonify(respnse={"error": "Record already exists"})
+    return render(request, 'cafeapp/add.html', {'form': form})
 
 
 def all(request):
