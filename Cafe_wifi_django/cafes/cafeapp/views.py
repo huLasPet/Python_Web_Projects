9716ab26c01e99189a386c055cd13959e485789b
huLasPet <kenwoodo@hotmail.com>
2022-04-13 11:30:17 +0200

    Cafe and Wi-Fi site Django - added delete function

--- Cafe_wifi_django/cafes/cafeapp/views.py
+++ Cafe_wifi_django/cafes/cafeapp/views.py
@@ -3,25 +3,31 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Cafe
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 
 def index(request):
     return render(request, 'cafeapp/index.html')
 
+
 def add(request):
     return render(request, 'cafeapp/add.html')
+
 
 def all(request):
     cafes = Cafe.objects.all()
     context = {'cafes': cafes}
     return render(request, 'cafeapp/all.html', context)
 
+
 def edit(request, id):
     cafe_id = Cafe.objects.get(pk=id)
     return render(request, 'cafeapp/edit.html')
 
+
 def delete(request, id):
     cafe_id = Cafe.objects.get(pk=id)
-    print(f"Deleted cafe called {cafe_id}")
+    cafe_id.delete()
     return HttpResponseRedirect(reverse('cafeapp:all'))
