174d325f33506bec51dcbae6d70f424b1b89a663
huLasPet <kenwoodo@hotmail.com>
2022-04-14 15:50:54 +0200

    Cafe and Wi-Fi site Django - added delete function

--- Cafe_wifi_django/cafes/cafeapp/views.py
+++ Cafe_wifi_django/cafes/cafeapp/views.py
@@ -16,19 +16,19 @@ def index(request):
 
 def add(request):
     form = CafeForm()
-    print(form.fields)
     if request.method == 'POST':
-        # cafe_to_add = Cafe(can_take_calls=form.fields.can_take_calls,
-        #                    coffee_price=form.fields.coffee_price.data,
-        #                    has_sockets=form.fields.has_sockets.data,
-        #                    has_toilet=form.fields.has_toilet.data,
-        #                    has_wifi=form.fields.has_wifi.data,
-        #                    img_url=form.fields.img_url.data,
-        #                    location=form.fields.location.data,
-        #                    map_url=form.fields.map_url.data,
-        #                    name=form.fields.name.data,
-        #                    seats=form.fields.seats.data)
-        print(request.POST["seats"])
+        cafe_to_add = Cafe(can_take_calls=request.POST["can_take_calls"],
+                           coffee_price=request.POST["coffee_price"],
+                           has_sockets=request.POST["has_sockets"],
+                           has_toilet=request.POST["has_toilet"],
+                           has_wifi=request.POST["has_wifi"],
+                           img_url=request.POST["img_url"],
+                           location=request.POST["location"],
+                           map_url=request.POST["map_url"],
+                           name=request.POST["name"],
+                           seats=request.POST["seats"])
+        print(cafe_to_add)
+        cafe_to_add.save()
         return render(request, 'cafeapp/index.html')
         # try:
         #     db_sqlalchemy.session.add(cafe_to_add)
