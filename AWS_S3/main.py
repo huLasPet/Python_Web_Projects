c184ba3e80c5336a4769424159d0998e8230fe0f
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-06-06 11:01:03 +0200

    Update main.py

--- AWS_S3/main.py
+++ AWS_S3/main.py
@@ -1,6 +1,6 @@
 import logging
 from boto3 import Session
-from botocore.exceptions import BotoCoreError, ClientError
+from botocore.exceptions import ClientError
 import os
 from dotenv import load_dotenv
 
@@ -27,6 +27,24 @@ class S3:
             return False
         return True
 
+    def list_buckets(self):
+        s3_client = self.session.client('s3')
+        response = s3_client.list_buckets()
+        print('Existing buckets:')
+        for bucket in response['Buckets']:
+            print(f'  {bucket["Name"]}')
+
+    def upload_file(self, bucket_name, source_file, target_name):
+        s3 = self.session.client('s3')
+        with open(source_file, "rb") as f:
+            s3.upload_fileobj(f, bucket_name, target_name,
+                              ExtraArgs={'ACL': 'public-read'})
+
+
 if __name__ == "__main__":
     aws_s3 = S3()
-    aws_s3.create_bucket(bucket_name="hulaspettest", region="eu-central-1")
+    #aws_s3.create_bucket(bucket_name="hulaspettest", region="eu-central-1")
+    #aws_s3.list_buckets()
+    #aws_s3.upload_file("hulaspettest",
+    #                   "/Users/nbyy/Library/CloudStorage/OneDrive-Personal/Pictures/Screenshots/OculusScreenshot1610295573.jpeg",
+    #                   "screenshot.jpeg")
